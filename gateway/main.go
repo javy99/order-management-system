d957f6ddf4503b6d1aeecbf232078c43d9399b2c
javy99 <kosimov.javlonbek@bk.ru>
2025-06-05 17:35:05 +0200

    interservice communication setup

--- gateway/main.go
+++ gateway/main.go
@@ -5,17 +5,31 @@ import (
 	"net/http"
 
 	_ "github.com/joho/godotenv/autoload"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	common "github.com/javy99/commons"
+	pb "github.com/javy99/commons/api"
 )
 
 var (
-	httpAddr = common.EnvString("HTTP_ADDR", ":8080")
+	httpAddr         = common.EnvString("HTTP_ADDR", ":8080")
+	orderServiceAddr = "localhost:2000"
 )
 
 func main() {
+	conn, err := grpc.NewClient(orderServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to dial server: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Dialing orders service at: ", orderServiceAddr)
+
+	c := pb.NewOrderServiceClient(conn)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoutes(mux)
 
 	log.Printf("Starting HTTP server at %s", httpAddr)
--- orders/main.go
+++ orders/main.go
@@ -1,10 +1,36 @@
 package main
 
-import "context"
+import (
+	"context"
+	"log"
+	"net"
+
+	common "github.com/javy99/commons"
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = common.EnvString("GRPC_ADDR", "localhost:2000")
+)
 
 func main() {
+	grpcServer := grpc.NewServer()
+
+	l, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	defer l.Close()
+
 	store := NewStore()
 	svc := NewService(store)
+	NewGRPCHandler(grpcServer)
 
 	svc.CreateOrder(context.Background())
+
+	log.Println("GRPC Server started at ", grpcAddr)
+
+	if err := grpcServer.Serve(l); err != nil {
+		log.Fatal(err.Error())
+	}
 }
